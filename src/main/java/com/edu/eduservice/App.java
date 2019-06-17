5efd2ce662f2b3dca8f506192df1f8fd79da21f5
olive <lhj_olive@163.com>
2019-06-17 15:15:37 +0800

    add

---
+++ src/main/java/com/edu/eduservice/App.java
@@ -0,0 +1,14 @@
+package com.edu.eduservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
